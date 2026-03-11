b57d120e30cfc5fd9f7eb79a025bff38de675f8c
adziedzic2127 <91894701+adziedzic2127@users.noreply.github.com>
2026-03-10 22:54:51 -0400

    Update build_all_branches.sh

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -11,6 +11,7 @@ touch base/.nojekyll
 # Generating documentation for each other branch in a subdirectory
 echo "All branches:"
 git fetch --all
+# already present
 echo "$(git branch --remotes --format '%(refname:lstrip=3)' | grep -Ev '^(HEAD|develop|gh-pages)$')"
 for BRANCH in $(git branch --remotes --format '%(refname:lstrip=3)' | grep -Ev '^(HEAD|develop|gh-pages)$'); do
     SANITIZED_BRANCH="$(echo $BRANCH | sed 's/\//_/g')"
